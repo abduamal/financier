2c01e71e9269cbf68c273dab5d44c3a6ff4d0930
Amal Abdu <abduamal@ymail.com>
2021-09-12 10:24:02 -0500

    syntax changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,11 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource(
-     '*',
-     headers: :any,
-     expose: ["Authorization"],
-     methods: [:get, :patch, :put, :delete, :post, :options, :show, :head]
-    )
+
+    resource '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
