e9e319ba784e794287345956d9098e798647eff0
Amal Abdu <abduamal@ymail.com>
2021-09-12 10:23:37 -0500

    add User Serializer + attributes for fastjson

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :middle_initial, :last_name, :created_at
+class UserSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :id, :email, :created_at
 end
