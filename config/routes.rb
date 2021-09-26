1659e6a60b194ac1f42f08268e78804fb2d228ad
Amal Abdu <abduamal@ymail.com>
2021-09-25 21:08:33 -0500

    add User, Accounts, and AccountTransactions resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   devise_for :users, path: '', path_names:{
     sign_in: 'login',
     sign_out: 'logout',
@@ -10,6 +9,12 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  resources :users do
+    resources :accounts do
+      resources :account_transactions
+    end
+  end
 
   get '/current_user', to: 'current_user#index'
   get 'private/test'
